6b2d0445555e5c933a11d6b7559f44e552040353
Sunny01 <whs3296116638@outlook.com>
2026-02-13 15:14:35 +0100

    make redis become global singleton instance

---
+++ api_platform/api-platorm-backend/admin/config/app_config.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from common.redis_util.redis_util import Redis
+from common.config_read.config_read import ConfigReader
+from common.jwt.jwt_utils import TokenUtill
+
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    # load application.yaml file
+    config_reader = ConfigReader()
+    config_data = config_reader.load()
+    #intilaize Redis
+    await Redis.initialize(host=config_data["redis"]["host"],port=config_data["redis"]["port"],
+                     db=config_data["redis"]["db"],password=config_data["redis"]["password"])
+    #intialize token utils
+    TokenUtill.init(secret_key=config_data["token"]["secret_key"])
+    print(f"{config_data['app']['name']} application started........")
+    yield
+    Redis.get_instance().close()
+    print(f"{config_data['app']['name']} application closed........")
